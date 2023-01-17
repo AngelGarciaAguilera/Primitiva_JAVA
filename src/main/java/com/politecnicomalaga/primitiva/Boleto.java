38fa374da2cb2c3e9c65bccb1f7ea64dd1cb0d96
Live session user <agaragu0708@g.educaand.es>
2023-01-17 17:14:03 +0100

    Completado

---
+++ src/main/java/com/politecnicomalaga/primitiva/Boleto.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.politecnicomalaga.primitiva;
+
+/**
+ *
+ * @author mint
+ */
+public class Boleto {
+    
+    private byte[] boletoGanador;
+    
+    public Boleto(){
+        boletoGanador = new byte[7];
+        for(byte i = 0; i < boletoGanador.length; i++){
+            boletoGanador[i] = (byte) (Math.random()*49+1); //Número random desde 1 a 49
+            for(byte j=0; j < i; j++){
+                if(boletoGanador[i] == boletoGanador[j]){
+                    i--; //Compara el número random actual con el anterior, si son iguales vuelve a randomizar el número actual
+                }
+            }
+        }
+    }
+    
+    public void ganadora(){
+        for(byte i = 0; i < boletoGanador.length; i++){
+            boletoGanador[i] = (byte) (Math.random()*49+1); //Número random desde 1 a 49
+            for(byte j=0; j < i; j++){
+                if(boletoGanador[i] == boletoGanador[j]){
+                    i--; //Compara el número random actual con el anterior, si son iguales vuelve a randomizar el número actual
+                }
+            }
+        }
+    }
+
+    public byte comprobar(byte[] boletoIn){
+        byte aciertos = 0;
+
+        for(byte i=0; i < boletoIn.length; i++){
+            for(byte j=0; j < boletoGanador.length - 1; j++){
+                if(boletoIn[i] == boletoGanador[j]){
+                    aciertos++; 
+                }
+            }
+        }
+        return aciertos;
+    }
+
+    public boolean comprobarComplementario(byte[] boletoIn){
+        boolean complementario = false;
+
+        for(byte i=0; i < boletoIn.length; i++){
+            if(boletoIn[i] == boletoGanador[6]){
+                complementario = true; 
+            }
+        }
+        return complementario;
+    }
+
+    public byte[] getBoletoGanador(){
+        return boletoGanador;
+    }
+}
---
+++ src/main/java/com/politecnicomalaga/primitiva/Primitiva.java
@@ -0,0 +1,135 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.politecnicomalaga.primitiva;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mint
+ */
+public class Primitiva {
+
+    public static void main(String[] args) {
+        
+        Boleto ganador = new Boleto();
+        byte[] boletoIn = new byte[6];
+        Scanner sc = new Scanner(System.in);
+
+        String opcion;
+        boolean menu = true;
+        byte numero;
+        boolean repetido = false;
+        boolean incorrecto = false;
+        
+        while(menu){
+            System.out.println("-------------------------------------------------");
+            System.out.println("");
+            System.out.println("OPCIONES:");
+            System.out.println("1. Sortear de nuevo.");
+            System.out.println("2. Introducir boleto.");
+            System.out.println("3. Comprobar boleto.");
+            System.out.println("4. Comprobar complementario.");
+            System.out.println("Otra tecla: SALIR.");
+            System.out.println("");
+            System.out.println("-------------------------------------------------");
+            System.out.println("Escoja la tarea a realizar:");
+            
+            opcion = sc.next();
+            sc.reset();
+            
+            System.out.println("-------------------------------------------------");
+            
+            switch (opcion) {
+                case "1":
+                    ganador.ganadora();
+                    System.out.println("El sorteo ha sido realizado con éxito.");
+                    break;//LISTO
+                    
+                case "2":
+                    System.out.println("A continuación, introduzca su boleto para, posteriormente, compararlo con el boleto ganador.");
+                    System.out.println("");
+
+                    for(byte i=0; i < boletoIn.length; i++){
+                        do{
+                            incorrecto = false;
+                            System.out.println("Introduce el " + (i+1) + "º número de tu boleto.");
+                            numero = sc.nextByte();
+
+                            for(byte j=0; j < i; j++){
+                                if(numero == boletoIn[j]){
+                                    repetido = true;
+                                }
+                            }
+                            
+                            if(numero < 1 || numero > 49){
+                                System.out.println("El número introducido debe estar entre 1 y 49. Vuelva a introducirlo.");
+                                incorrecto = true;
+                            }else if(repetido){
+                                System.out.println("El número introducido está repetido. Vuelva a introducirlo.");
+                                incorrecto = true;
+                            }
+                            else{
+                                boletoIn[i] = numero;
+                            }
+                            repetido = false;
+                        }while(incorrecto);
+                    }
+
+                break;//LISTO
+
+                case "3":
+                    System.out.println("Tu boleto es: " + boletoIn[0] + " " + boletoIn[1] + " " + boletoIn[2] + " " + boletoIn[3] + " " + boletoIn[4] + " " + boletoIn[5]);
+                    System.out.println("El boleto ganador es: " + Arrays.toString(ganador.getBoletoGanador()));
+                    System.out.println("");
+
+                    if(ganador.comprobar(boletoIn) == 0){
+                        System.out.println("Tu boleto no tiene ningún acierto.");
+                    }else if(ganador.comprobar(boletoIn) == 0 && ganador.comprobarComplementario(boletoIn)){
+                        System.out.println("Tu boleto no tiene ningún acierto más el complementario.");
+                    }else if(ganador.comprobar(boletoIn) == 1){
+                        System.out.println("Tu boleto tiene un acierto.");
+                    }else if(ganador.comprobar(boletoIn) == 1 && ganador.comprobarComplementario(boletoIn)){
+                        System.out.println("Tu boleto tiene un acierto más el complementario.");
+                    }else if(ganador.comprobar(boletoIn) == 2){
+                        System.out.println("Tu boleto tiene dos aciertos.");
+                    }else if(ganador.comprobar(boletoIn) == 2 && ganador.comprobarComplementario(boletoIn)){
+                        System.out.println("Tu boleto tiene dos aciertos más el complementario.");
+                    }else if(ganador.comprobar(boletoIn) == 3){
+                        System.out.println("Tu boleto tiene tres aciertos.");
+                    }else if(ganador.comprobar(boletoIn) == 3 && ganador.comprobarComplementario(boletoIn)){
+                        System.out.println("Tu boleto tiene tres aciertos más el complementario.");
+                    }else if(ganador.comprobar(boletoIn) == 4){
+                        System.out.println("Tu boleto tiene cuatro aciertos.");
+                    }else if(ganador.comprobar(boletoIn) == 4 && ganador.comprobarComplementario(boletoIn)){
+                        System.out.println("Tu boleto tiene cuatro aciertos más el complementario.");
+                    }else if(ganador.comprobar(boletoIn) == 5){
+                        System.out.println("Tu boleto tiene cinco aciertos.");
+                    }else if(ganador.comprobar(boletoIn) == 5 && ganador.comprobarComplementario(boletoIn)){
+                        System.out.println("Tu boleto tiene cinco aciertos más el complementario.");
+                    }else if(ganador.comprobar(boletoIn) == 6){
+                        System.out.println("Tu boleto tiene seis aciertos.");
+                    }
+
+                    break;//LISTO
+                    
+                case "4":
+                    if(ganador.comprobarComplementario(boletoIn)){
+                        System.out.println("El número complementario se encuentra en tu boleto.");
+                    }else{
+                        System.out.println("El número complementario no se encuentra en tu boleto.");
+                    }
+                    
+                    break;//LISTO
+
+                default:
+                    sc.close(); //Cierro la entrada de teclado.
+                    menu = false;
+                    break;
+            }
+        }
+    }
+}
